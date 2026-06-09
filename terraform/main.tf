0d1bc9b9c10210d1e15ee285fd36145cb6743733
Michael Ludvig (CyberCX) <michael.ludvig@cybercx.co.nz>
2026-06-10 11:21:50 +1200

    Update terraform versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.7"
+  required_version = ">= 1.14"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
   # Uncomment to enable S3 remote state:
